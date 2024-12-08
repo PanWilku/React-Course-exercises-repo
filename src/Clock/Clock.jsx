b1ea0ff72d9faec26cf7f9b465dd8a12519f7d56
Marcin Wilk <marcin.wilk.503@gmail.com>
2024-12-08 23:50:33 +0900

    added paragraph to clock

--- src/Clock/Clock.jsx
+++ src/Clock/Clock.jsx
@@ -1,42 +1,49 @@
-import { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 function Clock() {
-  const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    const intervalID = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
 
-    return () => {
-      clearInterval(intervalID);
-    };
-  }, []);
+    const [time, setTime] = useState(new Date());
 
-  function formatTime() {
-    let hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    const meridem = hours >= 12 ? "PM" : "AM";
 
-    hours = hours % 12 || 12;
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+    
+        return () => {
+            clearInterval(intervalID);
+        }
+        
+    }, []);
 
-    return `${padZero(hours)}: ${padZero(minutes)}: ${padZero(seconds)} ${padZero(meridem)}`;
-  }
+    function formatTime(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridem = hours >= 12 ? "PM" : "AM";
 
-  function padZero(number) {
-    if (number < 10) {
-      return "0" + number;
-    } else {
-      return number;
+        hours = hours % 12 || 12;
+
+        return(`${padZero(hours)}: ${padZero(minutes)}: ${padZero(seconds)} ${padZero(meridem)}`);
+
     }
-  }
 
-  return (
-    <>
-      <p>{formatTime()}</p>
-    </>
-  );
+    function padZero(number) {
+
+        if (number < 10) {
+            return("0" + number)
+        } else {
+            return(number);
+        }
+    }
+
+    return(
+        <>
+            <p>{formatTime()}</p>
+            <p>This is clock!</p>
+        </>
+    );
 }
 
-export default Clock;
+export default Clock
